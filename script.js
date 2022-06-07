28eb08d0cff36053d2f27fec6ec37782e57d3765
yanaaaacl <a9506862137@yandex.ru>
2022-06-07 13:38:38 +0300

    jhkli

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var name = prompt("Как вас зовут?");
-alert("Привет, " + name);
-  
-
-
+var words = ["программа", "макака", "прекрасный", "оладушек"];
+var word = words[Math.floor(Math.random() * words.length)];
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+var remainingLetters = word.length;
+while (remainingLetters > 0) {
+    alert(answerArray.join(" "));
+    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("Пожалуйста, введите только одну букву.");
+    } else {
+        for (var j = 0; j < word.length; j++) {
+            if(word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+}
+alert(answerArray.join(" "));
+alert("Отлично! Было загадано слово " + word);
