bdb2b5f4abcc25a69d96e3a1bdd71fabf91e8ec9
yanaaaacl <a9506862137@yandex.ru>
2022-06-10 14:50:23 +0300

    jquery

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var headingElement = document.getElementById("main-heading");
-            console.log(headingElement.innerHTML);
-            var newHeadingText = prompt("Введите новый заголовок:");
-            headingElement.innerHTML = newHeadingText;
+var newHeadingText = prompt("Введите новый заголовок:");
+$("#main-heading").text(newHeadingText);
+$("body").append("<p>Это новый элемент</p>");
+for(var i=0; i < 3; i++){
+    var hobby = prompt("Назови одно из своих хобби!");
+    $("body").append("<p>" + hobby + "</p>");
+}
+
+$("h1").fadeIn(1000);
+$("h1").hide(1000);
+$("body").append("<p>Жужалица</p>");
