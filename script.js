d95b55e675c0b93968ac280b6db40967752ba17b
yanaaaacl <a9506862137@yandex.ru>
2022-06-10 12:18:37 +0300

    js and jq

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-var words = ["программа", "макака", "прекрасный", "оладушек", "фронтенд", "оккупант",
-"редиска", "горох", "лук", "хинкали"];
-var word = words[Math.floor(Math.random() * words.length)];
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-var remainingLetters = word.length;
-
-while (remainingLetters > 0) {
-    alert(answerArray.join(" "));
-    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
-    if (guess === null) {
-        break;
-    } else if (guess.length !== 1) {
-        alert("Пожалуйста, введите только одну букву.");
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            if(word[j] === guess) {
-                
-                answerArray[j] = guess;
-                remainingLetters--;
-            }
-        }
-    }
-}
-alert(answerArray.join(" "));
-alert("Отлично! Было загадано слово " + word);
+var headingElement = document.getElementById("main-heading");
+            console.log(headingElement.innerHTML);
+            var newHeadingText = prompt("Введите новый заголовок:");
+            headingElement.innerHTML = newHeadingText;
