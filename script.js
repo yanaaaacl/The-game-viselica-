c60f7f85ad63b489a50076b21d67006db677ffae
yanaaaacl <a9506862137@yandex.ru>
2022-06-11 11:20:47 +0300

    hhh

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+var myFriends = ["Максим", "Женя"];
+for (var i = 0; i < myFriends.length; i++) {
+    
+    $("body").append("<p>" + myFriends[i] + " - лучший!" + "</p>");
+};
+
 var newHeadingText = prompt("Введите новый заголовок:");
 $("#main-heading").text(newHeadingText);
-$("body").append("<p>Это новый элемент</p>");
-for(var i=0; i < 3; i++){
-    var hobby = prompt("Назови одно из своих хобби!");
-    $("body").append("<p>" + hobby + "</p>");
-}
 
-$("h1").fadeIn(1000);
-$("h1").hide(1000);
-$("body").append("<p>Жужалица</p>");
+    $("h1").fadeTo(2000, 0.1);
+    
