29b6d96875c714e21bc6aa80a9373272351f76fa
yanaaaacl <a9506862137@yandex.ru>
2022-06-15 11:12:13 +0300

    игра найди клад дописана

---
+++ script.js
@@ -0,0 +1,50 @@
+var getRandomNumber = function(size) {
+    return Math.floor(Math.random() * size);
+};
+
+var getDistance = function(event, target) {
+    var diffX = event.offsetX - target.x;
+    var diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+var getDistanceHint = function(distance) {
+    if (distance < 10) {
+        return "Оббожешься!";
+    } else if (distance < 20) {
+        return "Очень горячо!";
+    }else if (distance < 40) {
+        return "Горячо";
+    }else if (distance < 80) {
+        return "Тепло";
+    }else if (distance < 160) {
+        return "Холодно";
+    }else {
+        return "Замерзнешь!";
+    }
+};
+
+var width = 400;
+var height = 400;
+var clicks = 0;
+
+var target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
+
+
+$("#map").click(function (event) {
+clicks++;
+
+
+var distance = getDistance(event, target);
+
+var distanceHint = getDistanceHint(distance);
+
+$("#distance").text(distanceHint);
+
+if (distance < 8) {
+    alert("Клад найден! Сделано кликов: " + clicks);
+}
+});
